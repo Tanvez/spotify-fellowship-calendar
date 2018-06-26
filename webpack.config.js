4166def7832e7d8b5991ea0d6ec5dfbc3bc60de7
Vesna Tan <tanvezi@gmail.com>
2018-06-26 13:20:26 -0400

    refactored

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+module.exports = {
+  entry: './client/index.js',
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  context: __dirname,
+  devtool: 'source-map',
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['react']
+      }
+    },
+    {
+      test: /\.css$/,
+      use: [
+        'style-loader',
+        'css-loader'
+      ]
+    }
+    ]
+  }
+};
