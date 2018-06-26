ca1fd09429044fd88b4cc315b8fbd5231221274d
Vesna Tan <tanvezi@gmail.com>
2018-06-26 00:53:56 -0400

    deploying again again

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { createStore, applyMiddleware } from 'redux'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import registerServiceWorker from './registerServiceWorker'
+
 
 
 import App from './App'
@@ -20,4 +20,3 @@ ReactDOM.render(
     <App />
   </Provider>, document.getElementById('root')
 )
-registerServiceWorker()
