2e45e69faf130001ab041b581f5a8f743b44a62f
Vesna Tan <tanvezi@gmail.com>
2018-07-06 10:30:21 -0400

    feat: added snapshot test to App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,34 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Enzyme, {shallow} from 'enzyme'
+import expect from 'expect'
 import App from './App';
-import { Provider } from 'react-redux'
-import thunkMiddleware from 'redux-thunk'
+import configureStore from 'redux-mock-store'
+import ReactSixteenAdapter from 'enzyme-adapter-react-16'
+Enzyme.configure({adapter: new ReactSixteenAdapter()})
 
-import { createStore, applyMiddleware } from 'redux'
+const initialState =
+[
+  {
+    'createdAt':"2018-06-27T20:23:52.691Z",
+    'description':"Running test",
+    'end':"2018-01-01T16:11:00.000Z",
+    'id':11,
+    'start':"2018-01-01T16:11:00.000Z",
+    'updatedAt':"2018-06-27T20:23:52.691Z",
+    'userId':1
+  }
+]
 
-import reducer from './store'
+const mockStore = configureStore()
+const store = mockStore(initialState)
+const wrapper = shallow(<App store={store} />)
+describe('Testing root App.js', ()=>{
+    it('renders without crashing', () => {
+      expect(wrapper.find('App')).toHaveLength(1)
+    })
 
-const middleware = applyMiddleware(thunkMiddleware)
-const store = createStore(reducer, middleware)
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store = {store}>
-    <App />
-  </Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+    it('Capturing Snapshot of App', () => {
+      expect(wrapper).toMatchSnapshot()
+  })
+  }
+)
---
+++ src/store/index.test.js
@@ -0,0 +1,5 @@
+// import configureMockStore from 'redux-mock-store';
+// import thunk from 'redux-thunk';
+// import moxios from 'moxios';
+// import expect from 'chai';
+
